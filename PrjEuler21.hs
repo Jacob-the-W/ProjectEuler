f70c0dad64fe83f05b7cfc084402f9802723fb37
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-11 02:56:59 -0500

    Update PrjEuler21.hs
    This was a second attempt

--- PrjEuler21.hs
+++ PrjEuler21.hs
@@ -1,5 +1,8 @@
+import Data.List
+
 main = print (pairsBelow 10000)
 
-propDivisors n = [m|m<-[1..(n-1)], mod n m==0 ]
+propDivisors n = (1:) $ nub $ concat [ [x, div n x] | x <- [2..limit], rem n x == 0 ]
+     where limit = (floor.sqrt.fromIntegral) n
 d n = sum ( propDivisors n )
 pairsBelow n = [(i,d i)| i<-[1..n-1], i /= d i, i == d (d i), i < d i]
