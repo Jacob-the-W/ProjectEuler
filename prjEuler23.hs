59661dd18decf66ef647f61f73557ccb6c701635
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-11 04:29:10 -0500

    Update prjEuler23.hs
    Divisor sums are now calculated with primes, a little bit faster, but marginal at best. 23s, from 24s before.

--- prjEuler23.hs
+++ prjEuler23.hs
@@ -1,32 +1,21 @@
-divisorSum :: Integer -> Integer  -- don't care about order because we're summing.
-divisorSum n =
-  let limit = floor (sqrt (fromIntegral n))
-  in if limit^2 == n
-     then let pairs          = unzip $ [(m,div n m)|m<-[1..limit-1], mod n m==0]
-              littleDivisors = fst pairs
-              middle         = [limit]
-              bigDivisors    = drop 1 $ snd pairs
-          in sum (littleDivisors++middle++bigDivisors)
-     else
-       let pairs          = unzip $ [(m,div n m)|m<-[1..limit], mod n m==0]
-           littleDivisors = fst pairs
-           middle         = []
-           bigDivisors    = drop 1 $ snd pairs
-       in sum (littleDivisors++middle++bigDivisors)
+import Data.List
+
+import Primes (sumPropDivisors)
 
 isAbundant :: Integer -> Bool
-isAbundant n = divisorSum n > n
+isAbundant n = (n /= 1) && (sumPropDivisors n > n)
 
-abundantList :: Integer -> [Integer]
-abundantList n = [x | x <- [1..n], isAbundant x]
+abundantList :: [Integer]
+abundantList = [x | x <- [12..28123], isAbundant x]
 
 isSumOfAbundants :: Integer -> Bool
-isSumOfAbundants n = any (\x -> isAbundant (n-x)) (abundantList n)
+isSumOfAbundants n = any (\x -> isAbundant (n-x)) (takeWhile (<=n) abundantList)
 
 sumNotSumOfAbundants :: Integer -> Integer
 sumNotSumOfAbundants n = sum [x | x <- [1..n], not (isSumOfAbundants x)]
 
 -- Call the sumNotSumOfAbundants function with a limit of 28123
+--I cheat by finding the maximum first
 main::IO()
 main = do
-  print $ sumNotSumOfAbundants 28123
+  print $ sumNotSumOfAbundants 20161
