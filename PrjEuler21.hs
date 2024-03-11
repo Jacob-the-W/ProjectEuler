d170b19520832e924a3201a32ee94c9851dd6c7f
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-11 03:10:05 -0500

    Update PrjEuler21.hs
    See the library function `amicablePairs` in Primes.hs for the real work on the algorithm here.

--- PrjEuler21.hs
+++ PrjEuler21.hs
@@ -1,19 +1,5 @@
-import Data.List
-import Primes (sumPropDivisors)
+import Primes (amicablePairs)
 
+solution = sum . takeWhile (not . null) . map (\(a,b)->filter (<=10000) [a,b]) $ amicablePairs
 
-d = sumPropDivisors
-pairsBelow n = [[i,d i]| i<-[1..n-1],d i < n, i < d i, i == d (d i)]
-solution = sum $ concat $ pairsBelow 10000
-
---lets try constructing the abundant numbers first, then feed that into i instead of [1..n-1].
-
-isAbundant n = d n > n
-
-abundants = filter isAbundant [1..]
-pairsBelow' n = [[i,d i]| i<-takeWhile (<n) abundants,d i < n, i == d (d i)]
-
-
-main = do
-  print solution
-  print $ pairsBelow' $ 10000
+main = print solution
