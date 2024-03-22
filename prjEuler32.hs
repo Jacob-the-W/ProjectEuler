676a828a897c82fd9f77f7e21557f870e7dcb939
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-22 18:05:56 -0500

    Update prjEuler32.hs
    Lots of read, show, read, changed to just needing to read once.
    
    Combined cases in one big test.

--- prjEuler32.hs
+++ prjEuler32.hs
@@ -1,27 +1,20 @@
 
 import Data.List
 
-panDigitals::[Int]
-panDigitals = map read (permutations "123456789")
---2,3,4 digits makes sense
---xx*yyy==zzzz case
-first x = read (take 2 (show x))::Int
-second x= read (take 3 (drop 2 (show x)))::Int
-rest x  = read (drop 5 (show x))::Int
---x*yyyy=zzzz case?
-first' x = read (take 1 (show x))::Int
-second' x = read (take 4 (drop 1 (show x)))::Int
-rest' x = read (take 4 (drop 5 (show x)))::Int
--- xxyy=zzzzz? 99 *99 = 9801, so impossible. 
+panDigitals = permutations "123456789"
 
-case1 = [x |x<-panDigitals, first x *second x==rest x]
-case2 = [x |x<-panDigitals, first' x *second' x==rest' x]
-products = map (read . (drop 5 . show)) (case1 ++ case2)
+test [a,b,c,d,e,f,g,h,i] = 
+  let first = read [a,b]::Int --xx*yyy==zzzz case
+      second = read [c,d,e]::Int
+      rest = read [f,g,h,i]::Int    
+      first' = read [a]::Int  --x*yyyy=zzzz case
+      second' = read [b,c,d,e]::Int
+      -- xxyy=zzzzz? 99 *99 = 9801, so impossible. 
+  in if first * second == rest || first' * second' == rest then rest else 0
+products = filter (/=0) $ test <$> panDigitals
 uniqueProducts = map head . group $ products
 solution = sum uniqueProducts
 
 main = do
- print case1
- print case2
- print products
+ print uniqueProducts
  print solution
