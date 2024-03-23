bfd1d967ebbdaa0b4b9c390c960407b9b0a1d2e0
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-23 00:21:57 -0500

    Update prjEuler46.hs
    Original solution was doing a lot of extra work and took 225ms. The next without extra work was 30-50ms range.
    
    Switching to unboxed array to hold all the information gets us down to 10 ms

--- prjEuler46.hs
+++ prjEuler46.hs
@@ -1,10 +1,23 @@
--- smallest odd composite that cannot be written as the sum of a prime and twice a square
+-- smallest odd composite that cannot be written as 
+-- the sum of a prime and twice a square
 
-import Primes (isPrime, composites)
+import Primes (sieveUA)
+import Data.Array.Unboxed (assocs, (!), UArray)
 
-oddComposites = filter odd composites
+arr :: UArray Int Bool
+arr = sieveUA 6000 
 
-check n = not $ any (\k -> isPrime (n - 2*k^2)) [1..ceiling (sqrt (fromIntegral n))]
+isPrime :: Int -> Bool
+isPrime 2 = True
+isPrime n = (n>2) && odd n && (arr ! (n `div` 2))
+
+oddComposites :: [Int]
+oddComposites = [2*i + 1 | (i, False) <- assocs arr]
+
+check :: Int -> Bool
+check n = not $ any (\k -> isPrime (n - 2*k^2)) [1..floor (sqrt (fromIntegral n / 2))]
+
+solution :: Int
 solution = head . filter check $ oddComposites
 
 main :: IO()
