4a19e868d6d13aaa185f1372bdfdc359001dd033
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-23 00:57:35 -0500

    Update prjEuler49.hs
    650ms down to 325ms.
    
    Probably the change from Int. Tried changing to list (!!) instead, but (IntMap.!) seems better

--- prjEuler49.hs
+++ prjEuler49.hs
@@ -1,18 +1,19 @@
-import Primes (primes,isPrime)
-import Data.List
-import qualified Data.IntMap as M
+import Primes (primes')
+import Data.List (group,sort)
 
-candidates = M.fromAscList . zip [0..] . takeWhile (<10000) $ dropWhile (<1000) primes
+isPrime n = 
+  let r = floor . sqrt $ fromIntegral n
+  in all (\p -> n `rem` p /=0) . takeWhile (<=r) $ primes'
 
--- if pi > pj let n = pi -pj, need to check pj - n = is prime
-isPermutations xs = 1 == length (nub $ map (sort . show) xs)
+candidates = takeWhile (<10000) $ dropWhile (<1000) primes'
+
+-- if pi > pj let n = pi -pj, need to check pj - n = pj - (pi - pj) = 2*pj - pi is prime
+isPermutations = null . drop 1 . group . map (sort . show)
 
 solution :: Int
-solution =  read $ concatMap show (reverse . last $
-  filter isPermutations $
-    map (\(a,b,c) -> [a,b,b-c]) $ filter (\(a,b,c) -> isPrime $ b-c)
-      [(candidates M.! i,candidates M.! j,candidates M.! i - candidates M.! j)|
-                            i<-[0..length candidates-1],j<-[0..(i-1)]])
+solution =  read $ concatMap show (reverse . last $ [[a,b,c]| i<-[0..length candidates-1],j<-[0..(i-1)],
+        let a = candidates !! i; b = candidates !! j; c = 2*b-a;,
+        isPrime c, isPermutations [a,b,c]])
 
 main :: IO()
 main = do
