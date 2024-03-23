0c38e2a1d7a750d8eb0d2edd2140790964e7af02
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-23 02:33:59 -0500

    Add files via upload

---
+++ prjEuler53.hs
@@ -0,0 +1,10 @@
+choose :: Integer -> Integer -> Integer
+choose n k =
+  if k < 0 || k > n then 0 else
+  let a = min k (n-k)
+  in product [n - a + 1..n] `div` product [1..a]
+
+solution = length $ [(n,r) | n<-[23..100],r<-[0..n],choose n r > 1000000]
+
+main :: IO()
+main = do print solution
