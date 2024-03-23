a27b0e92b25f432bce7bc46e59fab6abf989702e
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-23 01:16:33 -0500

    Update prjEuler49.hs
    Down to 70 ms :)

--- prjEuler49.hs
+++ prjEuler49.hs
@@ -1,19 +1,21 @@
 import Primes (primes')
 import Data.List (group,sort)
+import Data.IntMap qualified as M
 
 isPrime n = 
   let r = floor . sqrt $ fromIntegral n
   in all (\p -> n `rem` p /=0) . takeWhile (<=r) $ primes'
 
-candidates = takeWhile (<10000) $ dropWhile (<1000) primes'
+candidates = M.fromDistinctAscList . zip [0..] . takeWhile (<10000) $ dropWhile (<1000) primes'
 
 -- if pi > pj let n = pi -pj, need to check pj - n = pj - (pi - pj) = 2*pj - pi is prime
-isPermutations = null . drop 1 . group . map (sort . show)
+isPermutations = null . tail . group . map (sort . show)
 
 solution :: Int
-solution =  read $ concatMap show (reverse . last $ [[a,b,c]| i<-[0..length candidates-1],j<-[0..(i-1)],
-        let a = candidates !! i; b = candidates !! j; c = 2*b-a;,
-        isPrime c, isPermutations [a,b,c]])
+solution =  read $ show =<< last xs where
+  xs = [[c,b,a]| i<-[0..length candidates-1],j<-[0..(i-1)],
+        let a = candidates M.! i; b = candidates M.! j; c = 2*b-a;,
+        isPrime c, isPermutations [a,b,c]]
 
 main :: IO()
 main = do
