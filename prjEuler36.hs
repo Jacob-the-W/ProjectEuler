9cb9c410e460eb56547e7c9451eafc2d404d5836
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-22 20:59:20 -0500

    Update prjEuler36.hs
    Realized I didn't need to break it into cases, and can just generate two palindromes at a time for every number [1..999].
    
    1 -> [1, 11]
    12 -> [121, 1221].
    
    The older variant (generating palindrome by length and then concatenating) is nicer in that it is already sorted, but since we are summing at the end anyways, order does not matter. And the code is much shorter.

--- prjEuler36.hs
+++ prjEuler36.hs
@@ -1,57 +1,18 @@
-import Primes (digitsBase)
-{-- Find the sum of all numbers < 10^6 which are palindromic in base 10 and base 2.
 
+import Primes (digitsBase)
+{-- 
+Find the sum of all numbers < 10^6 which are palindromic in base 10 and base 2.
 Example: 585 = 1001001001_2
 --}
 
-{-- Analysis/explanation
-Generate palindromes of a given length by taking numbers with half as many 
-digits, and combining them with their reverse.
-
-We must consider two cases: odd and even length palindromes.
-
--- Odd case
-Call the number of digits n = 2k+1. We must create a generator for the
-first half of the palindrome, and then combine it later.
-
-Note n `div` 2 = k, which will not cover the middle digit.
-
-Use k+1 digits. Call p = (n+1) `div` 2 = (2k + 2) `div` 2 = k+1
-
-10^m has m+1 digits, so 10^(p-1) has p digits, and 10^p - 1 has p digits, and
-that will be the range.
-
-E.g., for 5 digits, we need (5+1) `div` 2 = 3 digits, so the range is [100..999], 
-and our combining function will map xyz -> xyzyx
-
--- Even case
-If the number of digits n = 2k, then n `div` 2 = k, k digits will suffice.
-
-Let p = n `div` 2 = 2k `div` 2 = k.
-Again, 10^m has m+1 digits, so 10^(p-1) has p digits, and 10^p - 1 has p digits.
-
-E.g., for 4 digits, we need 4 `div` 2 = 2 digits, so the range is [10..99], and
-our combining function will map xy -> xyyx.
--}
-palindromesLength :: Int -> [Int]
-palindromesLength n
-  | odd n = 
-      let p = (n+1) `div` 2 -- [1,3,5] -> [1,2,3]
-      in oddCombine <$> [10^(p - 1)..10^p - 1] -- [10^(2-1)..10^2-1] -> [10..99] is for 3 digits
-  | otherwise = 
-      let p = n `div` 2 -- [2,4,6] -> [1,2,3]
-      in evenCombine <$> [10^(p - 1)..10^p - 1] -- [10^(2-1)..10^2-1] -> [10..99] is for 4 digits
- where
-   oddCombine x = 
-     let (s, s') = (show x, drop 1 $ reverse s) -- 123 -> ("123", "21")
-     in read $ s ++ s' -- "123" ++ "21" -> 12321
-   evenCombine x = 
-     let (s, s') = (show x, reverse s) -- 123 -> ("123", "321")
-     in read $ s ++ s' -- "123" ++ "321" -> 123321
-    
-
+-- For each number 1..999, generate an odd and even length palindrome by 
+-- concatenating the number with its reverse, and the number with its reverse
+-- minus the first digit.  999 -> 999999, 99999
 palindromes :: [Int]
-palindromes = palindromesLength =<< [1..6]
+palindromes = flipper =<< [1..999] :: [Int] where
+  flipper x =
+    let (s, r, c) = (show x, reverse s, drop 1 r)
+    in read . (s ++) <$> [r,c]
 
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome ds = ds == reverse ds
