b54d4842302a55d585a2dcf500ba5a8d12239c9e
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-22 21:46:44 -0500

    Update prjEuler38.hs

--- prjEuler38.hs
+++ prjEuler38.hs
@@ -5,20 +5,19 @@ Since we must have more than one multiple, we can't take the biggest pandigital
 So check up to 4 digits since 5000*2 = 10000, i.e., for some 4 digit numbers it
  is possible to concatenate up to a length of 9
 --}
-multiples n = [k*n|k<-[1..9]]
-
+multiples n =  (n*) <$> [1..9]
 
 isPanDigital n = sort (show n) == "123456789"
 
-multsConcatToLength k n =
-  elem k $ scanl1 (+) $ map (length . show) $ multiples n
+multsConcatToLength k n = elem k . scanl1 (+) $ length . show <$> multiples n
 
-numberGenerator n =
+generator :: Int -> Int
+generator n =
   if multsConcatToLength 9 n
-  then read (take 9 $ concatMap show (multiples n))::Integer
+  then read . take 9 $ show =<< multiples n
   else 0
 
-solution = maximum (filter isPanDigital [numberGenerator n|n<-[1..9999]])
+solution = maximum . filter isPanDigital $ generator <$> [1..9999]
 
 main :: IO()
 main = do
