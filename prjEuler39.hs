70c4da0085ed2eaf9471cebd6b8cbeed8fd9f18d
Jacob-the-W <144130312+Jacob-the-W@users.noreply.github.com>
2024-03-22 22:01:34 -0500

    Update prjEuler39.hs

--- prjEuler39.hs
+++ prjEuler39.hs
@@ -4,11 +4,9 @@
 
 For which value of p ≤ 1000, is the number of solutions maximised?
 --}
-import Data.List
 
-isPythagorean :: (Num a, Eq a) => a -> a -> a -> Bool
-
-isPythagorean a b c = a^2 + b^2 == c^2 --
+isPythagorean :: Int -> Int -> Int -> Bool
+isPythagorean a b c = a^2 + b^2 == c^2 
 
 {--
 Note that
@@ -23,16 +21,20 @@ Then a + b + b +1 ~= 2b +1 ~= p. Then b must go up to (p-1)div2 in the worst cas
 More exactly, a + 2b + 1 = p for the worst case for b, so
 b = (p-1-a)/2. 
 --}
-perimeters :: [Integer]
+perimeters :: [Int]
 perimeters = [12..1000]
 
-solutionsForPerimeter :: Integral a => a -> [(a, a, a)]
-solutionsForPerimeter p = filter (\(a,b,c) -> isPythagorean a b c) 
-                            [(a,b,p-a-b)|a<-[3..(p `div` 3)],b<-[a..(p-a-1) `div` 2]]
+solutionsForPerimeter :: Int -> [(Int,Int,Int)]
+solutionsForPerimeter p = 
+  [(a,b,c)| a<-[3..(p `div` 3)],
+    b<-[a+1..(p-a-1) `div` 2],
+    let c = p - a - b, isPythagorean a b c]
 
-lengthOfSolutions p = length $ solutionsForPerimeter p 
+lengthOfSolutions :: Int -> Int
+lengthOfSolutions = length . solutionsForPerimeter 
 
-solution = maximumBy (\(a,_) (b,_) -> compare a b) $ filter (\(a,_) -> a /= 0) [(lengthOfSolutions p,p)|p<-perimeters]
+solution :: Int
+solution = snd . maximum . flip zip perimeters $ lengthOfSolutions <$> perimeters
 
 main::IO()
 main = do
