21ad3a825da54b418c35925f83451b484a537530
Jacob-The-W <jacob.wa1993@gmail.com>
2024-03-31 21:43:49 -0500

    minor changes

--- app/prjEuler38.hs
+++ app/prjEuler38.hs
@@ -7,18 +7,18 @@ Since we must have more than one multiple, we can't take the biggest pandigital
 So check up to 4 digits since 5000*2 = 10000, i.e., for some 4 digit numbers it
  is possible to concatenate up to a length of 9
 --}
-multiples n =  (n*) <$> [1..9]
 
+isPanDigital :: Int -> Bool
 isPanDigital n = sort (show n) == "123456789"
 
-multsConcatToLength k n = elem k . scanl1 (+) $ length . show <$> multiples n
-
 generator :: Int -> Int
-generator n =
-  if multsConcatToLength 9 n
-  then read . take 9 $ show =<< multiples n
-  else 0
+generator n 
+  | multsConcat = read . take 9 $ concat multiples
+  | otherwise = 0 where 
+  multiples = show . (n*) <$> [1..9]
+  multsConcat = elem 9 . scanl1 (+) $ length <$> multiples
 
+solution :: Int
 solution = maximum . filter isPanDigital $ generator <$> [1..9999]
 
 main :: IO()
--- app/prjEuler42.hs
+++ app/prjEuler42.hs
@@ -5,20 +5,30 @@ import Data.Char ( ord )
 scoreLetter :: String -> [Int]
 scoreLetter = map (\x -> ord x - 64)
 
-triangle :: [Int]
-triangle = scanl1 (+) [1..]
+scoreWord :: String -> Int
+scoreWord = sum . scoreLetter
 
+triangles :: [Int]
+triangles = map (\n -> n*(n+1) `div` 2) [0..]
+
+-- | Invert a triangular number, noticed this after doing 45
+-- n*(n+1)/2 = t
+-- n^2 + n - 2t = 0
+-- n = (-1 + sqrt(1+8t))/2 must be an integer, 
+-- so -1 + sqrt (1 +8t) must be even,
+-- sqrt ( 1 + 8t) must be odd
+-- (1 + 8t) must be odd, which we get for free
+-- Make sure 1+8t is a perfect square.
 triangular :: Int -> Bool
-triangular s = s `elem` takeWhile (<=s) triangle
+triangular s = 
+  let rad = 1 + 8*s
+      r = floor . sqrt . fromIntegral $ rad
+  in r*r == rad
 
-main::IO()
+main :: IO ()
 main = do
-  list <- readFile "data\\p042_words.txt"
-  let readList = read ("["++list++"]")
-  let scores = map (sum . scoreLetter) readList
-  putStrLn "Here's the list with all the scores for each name."
-  print $ zip readList scores
-  putStrLn "And here's the scores which are triangular numbers:"
-  print $ filter triangular scores
-  putStrLn "Length of that list"
-  print $ length $ filter triangular scores
+  input <- readFile "data\\p042_words.txt"
+  let list = read $ "[" ++ input ++ "]"
+      scores = filter triangular $ scoreWord <$> list
+      solution = length scores
+  print solution
